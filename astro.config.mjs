d535d4f52a50f7a96c031789ae82ae88445a00f7
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2023-05-19 15:28:42 +0530

    lmao rush

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // output: "server",
+  // adapter: vercel(),
+  compressHTML: true,
+  site: "https://beta.hrushalnikhare.xyz",
+  integrations: [mdx(), sitemap()],
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'HPC | Hackclub Pune';
+export const SITE_DESCRIPTION = '';
