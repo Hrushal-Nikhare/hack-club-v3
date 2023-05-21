edc1b71b316a71ef02056bf946a11aac35629c34
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2023-05-21 18:15:18 +0530

    The animation isnt working ;-;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-// import vercel from "@astrojs/vercel/serverless";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: vercel(),
   compressHTML: true,
   site: "https://beta.hrushalnikhare.xyz",
-  integrations: [mdx(), sitemap(), image()]
+  integrations: [sitemap(), image()]
 });
